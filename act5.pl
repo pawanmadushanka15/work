032efa32d7a25efcb43f633819060b4499ffcccd
Alandara Acharige Pawan Madushanka Perera <2022csc003@univ.jfn.ac.lk>
2025-07-15 18:38:53 +0530

    new

--- act5.pl
+++ act5.pl
@@ -1,4 +1,4 @@
-my $line="cat,dog,bird";
-my @animal=split(",",$line);
-print join(",",@animal),"\n";
-print $animal[1],"\n";
+print "Enter your name:";
+my $name=<STDIN>;
+chomp($name);
+print "Hello,$name!\n";
