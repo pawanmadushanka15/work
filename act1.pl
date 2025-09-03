0d711e238e370ca168bccf2d53388339733d8588
Pawan Madushanka <2022csc003@univ.jfn.ac.lk>
2025-09-03 08:57:48 +0530

    Add files via upload
    new

--- act1.pl
+++ act1.pl
@@ -1,51 +1,70 @@
-#if examples
-my @skills=("Perl","Python","Java","Unix","Shell");
-
-if ($skills[-1] eq "Shell")
-{
-	print "if statment true";
-}
-else{
-	print "if Statement false";
+my @skills=qw(Perl Python Unix);
+foreach (0..$#skills){
+    print "$skills[$_]\n";
 }
 
-print "\n";
+print "-------\n";
 
-#else if examples
-if ($skills[-3] eq "Shell")
-{
-	print "if statment true";
-}
-elsif($skills[0] eq "Perl"){
-	print "if Statement false";
-}
-else{
-	print "Else true";
+foreach my $i(0..$#skills){
+    print "$skills[$i]\n";
 }
 
-print "\n";
-#perl nested if 
-if($skills[0] eq "Perl")
-{
-	if($skills[-1] eq "Shell")
-	{
-		print "Both statement are true";
-	}
+print "-------\n";
+
+my $i=1;
+while ($i<5){
+    print "$i";
+    $i++;
 }
 
-print "\n";
-#unless examples
-unless(scalar @skills==5){
-	print "Condition is false";
-}else{
-	print "Condition is True";
+print "\n-------\n";
+
+my $i=10;
+do{
+    print "$i";
+    $i++
+}while($i<5);
+
+print "\n-------\n";
+
+my $i=1;
+until($i>3){
+    print "$i";
+    $i++;
 }
 
-print "\n";
-unless(scalar @skills==5){
-	print "Condition is false";
-}elsif($skills[0] eq "Perl"){
-	print /$skills[0] element is '$skills[0]'";
-}else{
-	print "Done!";
+print "\n-------\n";
+
+sub PrintList{
+    my @list=@_;
+    print "Given list is @list\n";
+    print "Second list element is @list[1]\n";
+    
 }
+
+$x=10;
+@y=(1,2,3,4);
+PrintList($x,@y);
+
+print "\n-------\n";
+
+sub sum{
+    my($a,$b)=@_;
+    my $C=$a+$b;
+}
+my $sum=sum(10,20);
+print "$sum";
+
+print "\n-------\n";
+
+my $text="I like apples";
+$text=~s/apples/oranges/;
+print "$text";
+
+print "\n-------\n";
+
+my $fruit="banana";
+my $sentence="I like banana pie";
+
+$senetence=~s/$fruit/apple/;
+print $sentence;
--- act2.pl
+++ act2.pl
@@ -1,82 +1,25 @@
-print "Enter a number:";
-my $number=<STDIN>;
-chomp($number);
-if ($number%2==0){
-	print "This is an even number";
+print "Enter nucleotide sequence (A/T/G/C): ";
+my $seq1 = <STDIN>;
+chomp($seq1);
+if ($seq1 !~ /^[ATGC]+$/i){
+    die "Invalid nucleotide! Please enter only A, T, G, or C\n";
 }
-else{
-	print "This is an odd number";
+
+print "\n-------\n";
+
+print "Enter nucleotide to delete (A/T/G/C): ";
+my $del = <STDIN>;
+chomp($del);
+if ($del !~ /^[ATGC]$/i){
+    die "Invalid nucleotide! Please enter only A, T, G, or C\n";
 }
-print "\n";
+
+print "\n-------\n";
+
+my $new_seq1 = $seq1;
+$new_seq1 =~ s/$del//gi;
+
+print "\nOriginal sequence: $seq1\n";
+print "Modified sequence: $new_seq1\n";
 
 
-print "Enter mark:";
-my $mark=<STDIN>;
-chomp($mark);
-if ($mark>=50){
-	print "Pass";
-}
-print "\n";
-
-print "Enter number:";
-my $number=<STDIN>;
-chomp($number);
-unless ($number<0){
-	print "Positive number";}
-print "\n";
-
-
-print "Enter mark:";
-my $mark=<STDIN>;
-chomp($mark);
-if ($mark>=70){
-	print "High";
-}elsif (30<=$mark<=69){
-	print "Medium";
-}else{
-	print "Low";
-}
-print "\n";
-
-print "Enter string:";
-my $str=<STDIN>;
-chomp($str);
-unless ($str eq ""){
-	print "String";
-}else{
-	print "empty string";
-}
-print "\n";
-
-print "Enter age:";
-my $age=<STDIN>;
-chomp($age);
-if ($age>=18)
-{
-	print "Do you have an ID:";
-	my $ID=<STDIN>;
-	chomp($ID);
-	unless ($ID eq "yes")
-	{
-		print "NO ID";
-	}
-}
-print "Too young";
-print "\n";
-
-print "Enter username:";
-my $username=<STDIN>;
-chomp($username);
-print "Enter password:";
-my $password=<STDIN>;
-chomp($password);
-unless ($username eq "" or $password eq ""){
-	if ($username eq "admin" and $password eq "1234"){
-		print "Acess granted";
-	}else{
-		print "Acess denied";
-	}
-}else
-{
-	print "Missing Credentials";
-}
